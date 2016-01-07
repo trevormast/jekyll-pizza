a20f3d01fad3de4fbf826293aa6d08f9a6377a87
trevormast <trevormast@trevormasts-MBP.westell.com>
2016-01-06 16:50:46 -0800

    WIP

--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -1,8 +1,31 @@
 require 'spec_helper'
 
 describe 'App' do
-	it 'should get the homepage' do
-		get '/'
-		expect(last_response).to be_ok
-	end
+	
+	describe "GET '/'" do
+    it "loads homepage" do
+      get '/'
+      expect(last_response).to be_ok
+    end
+   end
+	
+	describe "GET '/new'" do
+    it "loads new page" do
+      get '/new'
+      user = double('user')
+  		allow(request(7).env['warden']).to receive(:authenticate!).and_return(user)
+  		allow(controller).to receive(:current_user).and_return(user)
+      # stub_request(:get, "www.github.com").to_return(Warden::GitHub::User.new)
+      expect(last_response).to be_ok
+    end
+  end
+
+ describe "GET '/create'" do
+    it "redirects to /new page" do
+      get '/create'
+      expect(last_response).to be_redirect
+      follow_redirect!
+      expect(last_request.url).to eq('http://example.org/new')
+    end
+  end
 end
