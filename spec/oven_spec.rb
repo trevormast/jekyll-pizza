86394bfb0eaf9839b9f0b37ee427e59b8a0f1742
trevormast <trevormast@gmail.com>
2016-03-10 13:40:48 -0800

    Refactors commit process in Oven

--- spec/oven_spec.rb
+++ spec/oven_spec.rb
@@ -30,7 +30,7 @@ describe 'Oven' do
 
     @oven.run(order: @order, dir: @dir)
 
-    allow(@oven).to receive(:commit_new_jekyll)
+    # allow(@oven).to receive(:commit_new_jekyll)
 
     # TODO: stub the commit_new_jekyll method to stop ext requests
     
@@ -40,7 +40,15 @@ describe 'Oven' do
     expect(@oven.user.attribs).to include('login')
   end
 
-  # it 'has a full repo name' do
-  #   expect(@oven.repo).to respond_to(:full_name)
-  # end
+  it 'has a full repo name' do
+    expect(@oven.repo).to respond_to(:full_name)
+  end
+
+  it 'creates a repo info struct' do
+    repo = @oven.create_repo_object(@order.user.api.create_repository(@repo_url, auto_init: true))
+
+    expect(repo.full_repo_path).to eq('test_user/path')
+    expect(repo.sha_latest_commit).to eq('c0879ec586f927218eb41e5e51578afc0e71cd10')
+    expect(repo.sha_base_tree).to eq('4dd7b0f95a42943a3642007bde90e3f652689b73')
+  end
 end
