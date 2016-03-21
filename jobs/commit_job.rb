1b2e5261a5cc5a3e6338ce7354d10b960b640564
trevormast <trevormast@gmail.com>
2016-03-21 15:06:31 -0700

    Extends job timeout for dev and testing

--- jobs/commit_job.rb
+++ jobs/commit_job.rb
@@ -4,15 +4,18 @@ require './lib/recipe'
 require './lib/oven'
 require './lib/taste_test'
 
-SuckerPunch.shutdown_timeout = 1500
+unless ENV['RACK_ENV'] == 'production'
+  SuckerPunch.shutdown_timeout = 1500 
+end
 
 class CommitJob
   include SuckerPunch::Job
 
   def perform(order)
     JekyllPizza::Delivery.new(order: order, 
-                              directory: JekyllPizza::Recipe.new(order.site_params).dir,
-                              repo: JekyllPizza::Oven.new, 
-                              build_status: JekyllPizza::TasteTest.new).run
+                 directory: JekyllPizza::Recipe.new(order.site_params).dir,
+                 repo: JekyllPizza::Oven.new, 
+                 build_status: JekyllPizza::TasteTest.new).run
   end
 end
+
