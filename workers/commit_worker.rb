b2c9af33d050f756b85513182ea450af1c5d6ab7
trevormast <trevormast@gmail.com>
2016-03-31 15:00:40 -0700

    Adds sidekiq_status gem to make worker status available

--- workers/commit_worker.rb
+++ workers/commit_worker.rb
@@ -1,4 +1,5 @@
 require 'sidekiq'
+require 'sidekiq_status' if ENV['RACK_ENV'] != 'test'
 require 'octokit'
 require 'active_support'
 require 'active_support/core_ext/object/blank'
@@ -9,27 +10,28 @@ require './lib/oven'
 require './lib/taste_test'
 require 'pry'
 
-Sidekiq.configure_server do |config|
-  if ENV['RACK_ENV'] == 'production'
-    config.redis = { url: 'redis://redistogo:df7be5588f72a6fe0e7b93dfb97f1464@squawfish.redistogo.com:9248/' }
-  end
-end
+# Sidekiq.configure_server do |config|
+#   if ENV['RACK_ENV'] == 'production'
+#     config.redis = { url: 'redis://redistogo:df7be5588f72a6fe0e7b93dfb97f1464@squawfish.redistogo.com:9248/' }
+#   end
+# end
 
-Sidekiq.configure_client do |config|
-  if ENV['RACK_ENV'] == 'production'
-    config.redis = { url: 'redis://redistogo:df7be5588f72a6fe0e7b93dfb97f1464@squawfish.redistogo.com:9248/' }
-  end
-end
+# Sidekiq.configure_client do |config|
+#   if ENV['RACK_ENV'] == 'production'
+#     config.redis = { url: 'redis://redistogo:df7be5588f72a6fe0e7b93dfb97f1464@squawfish.redistogo.com:9248/' }
+#   end
+# end
 
 class CommitWorker
   include Sidekiq::Worker
+  include SidekiqStatus::Worker if ENV['RACK_ENV'] != 'test'
+  # sidekiq_options retry: false
 
   def perform(token, params)
     client = Octokit::Client.new(access_token: token)
 
     order = JekyllPizza::Order.new(user: client,
                                    params: params)
-
     JekyllPizza::Delivery.new(order: order, 
                               directory: JekyllPizza::Recipe.new(order.site_params).dir,
                               repo: JekyllPizza::Oven.new, 
