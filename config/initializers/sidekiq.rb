f398b00dd101fe3ca74a4d6974058a4797bf908b
trevormast <trevormast@gmail.com>
2016-06-30 15:42:43 -0700

    Adds url for redis in production

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,13 +1,9 @@
 require 'sidekiq'
 
 Sidekiq.configure_client do |config|
-  config.redis = { size: 1 }
+  config.redis = { size: 1, url: REDISTOGO_URL }
 end
 
 Sidekiq.configure_server do |config|
-  # The config.redis is calculated by the
-  # concurrency value so you do not need to
-  # specify this. For this demo I do
-  # show it to understand the numbers
-  config.redis = { size: 9 }
+  config.redis = { size: 9, url: REDISTOGO_URL }
 end
